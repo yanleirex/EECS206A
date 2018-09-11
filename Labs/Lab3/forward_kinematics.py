ef9b3ad93e3f659fc13ff0e649daeceef48d8779
Jun Zeng <zengjunsjtu@berkeley.edu>
2018-09-10 20:26:42 -0700

    Lab3 completed

--- Labs/Lab3/forward_kinematics.py
+++ Labs/Lab3/forward_kinematics.py
@@ -33,7 +33,7 @@ def rotationMatrixToEulerAngles(R) :
      
     singular = sy < 1e-6
  
-    if  not singular :
+    if  not singular:
         x = math.atan2(R[2,1] , R[2,2])
         y = math.atan2(-R[2,0], sy)
         z = math.atan2(R[1,0], R[0,0])
