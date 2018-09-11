973eb8c25b3dc92bfd17357e4b5f96c381e2ca21
Jun Zeng <zengjunsjtu@berkeley.edu>
2018-09-10 17:39:25 -0700

    tf2 listener completed

--- Labs/Lab3/tf_echo.py
+++ Labs/Lab3/tf_echo.py
@@ -1,5 +1,19 @@
-import tf2_ros
+#!/usr/bin/env python
 
-tfBuffer = tf2_ros.Buffer()
-tfListener = tf2_ros.TransformListener(tfBuffer)
-trans = tfBuffer.lookup_transform(target_frame, source_frame, rospy.Time())
+import tf2_ros
+import rospy
+import math
+import tf
+from std_msgs.msg import String
+from geometry_msgs.msg import TransformStamped 
+
+if __name__ == '__main__':
+	rospy.init_node('listener')
+	tfBuffer = tf2_ros.Buffer()
+	tfListener = tf2_ros.TransformListener(tfBuffer)
+	while not rospy.is_shutdown():
+		try:
+			trans = tfBuffer.lookup_transform('left_hand', 'base', rospy.Time())
+			print(trans.transform)
+		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+			continue
